b9c1aae6b139fc869b34e15940e59bb34d727d55
0xilis <109871561+0xilis@users.noreply.github.com>
2023-10-06 02:51:51 -0400

    fix accidental space

--- c.sh
+++ c.sh
@@ -1,3 +1,3 @@
 clang -o Unsigncuts -Wall main.c -Oz -flto
 strip Unsigncuts
-clang  -o mod.dylib -Wall -lobjc -dynamiclib dylib.c -Oz -flto
+clang -o mod.dylib -Wall -lobjc -dynamiclib dylib.c -Oz -flto
