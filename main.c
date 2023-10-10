0f1721526aaba9a7e020edd371ddac6318a1f99d
0xilis <109871561+0xilis@users.noreply.github.com>
2023-10-09 23:34:47 -0400

    optim: one malloc to rule them all

--- main.c
+++ main.c
@@ -23,37 +23,31 @@ int main(int argc, char **argv) {
    if (runPathLen == UINT_MAX) { goto res_err; };
    if (runPath[runPathLen] != '/') { goto res_find; };
    #else
+   /* yes, we take into the account the two extra }'s */
    for (int runPathLen = strlen(runPath); runPathLen >= 0; runPathLen--) {
     if (runPath[runPathLen] == '/') {
    #endif
-   /* Copy into resourcesPath */
-   /* Cut the beginning and ending " */
-   char *start = &runPath[0];
-   /* Note the + 1 here, to have a null terminated substring */
    runPathLen += 1;
-   char *pathToDylib = (char *)malloc(runPathLen + 9);
-   memcpy(pathToDylib, start, runPathLen);
+   unsigned int dylibPathLen = 9 + runPathLen + 22; /* +1 bc null byte, 22 for DYLD_INSERT_LIBRARIES= */
+   char* dylibPath = malloc(sizeof(char) * dylibPathLen+1);
+   strncat(dylibPath, "DYLD_INSERT_LIBRARIES=", 22);
+   strncat(dylibPath, runPath, runPathLen);
    /* find_resource */
-   unsigned int pathToDylibLen = 9+runPathLen;
+   runPathLen += 22;
    char *resource = "mod.dylib";
    /* Copy resource to pathToDylib */
    #if __x86_64__
    int i = 0;
    hacky_optimized_for_loop:
-   pathToDylib[i+runPathLen] = resource[i];
+   dylibPath[i+runPathLen] = resource[i];
    i++;
    if (i != 9) { goto hacky_optimized_for_loop; };
    #else
    for (int i = 0; i < 9; i++) {
-    pathToDylib[i+runPathLen] = resource[i];
+    dylibPath[i+runPathLen] = resource[i];
    }
    #endif
-   pathToDylib[pathToDylibLen] = '\0';
-   unsigned int dylibPathLen = pathToDylibLen + 23; /* +1 bc null byte, 22 for DYLD_INSERT_LIBRARIES= */
-   char* dylibPath = malloc(sizeof(char) * dylibPathLen);
-   strncat(dylibPath, "DYLD_INSERT_LIBRARIES=", 22);
-   strncat(dylibPath, pathToDylib, pathToDylibLen);
-   free(pathToDylib);
+   dylibPath[dylibPathLen] = '\0';
    /* dunno how this works but it does */
    char *const *envp = &dylibPath;
    posix_spawn(NULL, SC_PATH, NULL, NULL, NULL, envp);
