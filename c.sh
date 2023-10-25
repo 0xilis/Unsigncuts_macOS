93e128700daca03cf3f10c61c80d96c01b87daf2
0xilis <109871561+0xilis@users.noreply.github.com>
2023-10-24 21:47:56 -0400

    optim: emit frame pointer to speed up mod.dylib ctor

--- c.sh
+++ c.sh
@@ -1,3 +1,3 @@
 clang -o Unsigncuts -Wall main.c -Oz -flto
 strip Unsigncuts
-clang  -o mod.dylib -Wall -lobjc -dynamiclib dylib.c -Oz -flto -nostdlib
+clang  -o mod.dylib -Wall -lobjc -dynamiclib dylib.c -Oz -flto -nostdlib -fomit-frame-pointer
